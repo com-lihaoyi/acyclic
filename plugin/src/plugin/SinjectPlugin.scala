54ee395a9b78b17eff1c3e47898c9c1230927d1f
Li Haoyi <haoyi.sg@gmail.com>
2012-11-30 18:04:24 -0500

    Changed some stuff

--- plugin/src/plugin/SinjectPlugin.scala
+++ plugin/src/plugin/SinjectPlugin.scala
@@ -5,11 +5,8 @@ import tools.nsc.plugins.{Plugin, PluginComponent}
 
 class SinjectPlugin(val global: Global) extends Plugin {
 
-
-  val AutoproxyAnnotationClass = "autoproxy.annotation.proxy"
-
-  val name = "autoproxy"
-  val description = "support for the @proxy annotation"
+  val name = "Sinject"
+  val description = "Automatically creates implicit parameters"
 
   val components = List[PluginComponent](
     new SinjectTransformer(this, global)
