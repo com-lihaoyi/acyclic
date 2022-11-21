d06e7f99723e67696e689bcfffed43427c21cfb2
Tobias Roeser <le.petit.fou@web.de>
2022-11-21 21:06:31 +0100

    Replaced deprecated utest API

--- acyclic/test/src/acyclic/CycleTests.scala
+++ acyclic/test/src/acyclic/CycleTests.scala
@@ -9,19 +9,19 @@ import acyclic.file
 
 object CycleTests extends TestSuite {
 
-  def tests = TestSuite {
-    'fail {
-      'simple - makeFail("fail/simple")(Seq(
+  def tests = Tests {
+    test("fail") - {
+      test("simple") - makeFail("fail/simple")(Seq(
         File("B.scala") -> SortedSet(4, 5),
         File("A.scala") -> SortedSet(5)
       ))
 
-      'indirect - makeFail("fail/indirect")(Seq(
+      test("indirect") - makeFail("fail/indirect")(Seq(
         File("A.scala") -> SortedSet(6),
         File("B.scala") -> SortedSet(3),
         File("C.scala") -> SortedSet(4)
       ))
-      'cyclicgraph - makeFail("fail/cyclicgraph")(
+      test("cyclicgraph") - makeFail("fail/cyclicgraph")(
         Seq(
           File("A.scala") -> SortedSet(5),
           File("E.scala") -> SortedSet(6),
@@ -29,38 +29,38 @@ object CycleTests extends TestSuite {
           File("C.scala") -> SortedSet(4, 5)
         )
       )
-      'cyclicpackage - makeFail("fail/cyclicpackage")(
+      test("cyclicpackage") - makeFail("fail/cyclicpackage")(
         Seq(
           Pkg("fail.cyclicpackage.b") -> SortedSet(5),
           Pkg("fail.cyclicpackage.a") -> SortedSet(5)
         )
       )
-      'halfpackagecycle - makeFail("fail/halfpackagecycle")(Seq(
+      test("halfpackagecycle") - makeFail("fail/halfpackagecycle")(Seq(
         File("B.scala") -> SortedSet(3),
         File("A.scala") -> SortedSet(4),
         Pkg("fail.halfpackagecycle.c") -> SortedSet(5)
       ))
     }
-    'success {
-      'simple - make("success/simple")
-      'ignorejava - make("success/java")
-      'cyclicunmarked - make("success/cyclicunmarked")
-      'dag - make("success/dag")
-      'pkg {
-        "single" - make("success/pkg/single")
-        "mutualcyclic" - make("success/pkg/mutualcyclic")
-        "halfacyclic" - make("success/pkg/halfacyclic")
-        "innercycle" - make("success/pkg/innercycle")
+    test("success") - {
+      test("simple") - make("success/simple")
+      test("ignorejava") - make("success/java")
+      test("cyclicunmarked") - make("success/cyclicunmarked")
+      test("dag") - make("success/dag")
+      test("pkg") {
+        test("single") - make("success/pkg/single")
+        test("mutualcyclic") - make("success/pkg/mutualcyclic")
+        test("halfacyclic") - make("success/pkg/halfacyclic")
+        test("innercycle") - make("success/pkg/innercycle")
       }
     }
-    'self - make("../../src", extraIncludes = Nil)
-    'force {
-      'fail - makeFail("force/simple", force = true)(Seq(
+    test("self") - make("../../src", extraIncludes = Nil)
+    test("force") - {
+      test("fail") - makeFail("force/simple", force = true)(Seq(
         File("B.scala") -> SortedSet(4, 5),
         File("A.scala") -> SortedSet(4)
       ))
-      'pass - make("force/simple")
-      'skip - make("force/skip", force = true)
+      test("pass") - make("force/simple")
+      test("skip") - make("force/skip", force = true)
     }
   }
 }
