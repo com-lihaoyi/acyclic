c032bc725a8acd0668a1a32b38021382b39b734e
Matt Dziuban <mrdziuban@gmail.com>
2024-09-22 22:19:50 -0400

    Prevent duplicate runs.

--- acyclic/src-3/acyclic/plugin/Plugin.scala
+++ acyclic/src-3/acyclic/plugin/Plugin.scala
@@ -13,12 +13,18 @@ class TestPlugin(cycleReporter: Seq[(Value, SortedSet[Int])] => Unit = _ => ()) 
 
   var force = false
   var fatal = true
+  var alreadyRun = false
 
   private class Phase() extends PluginPhase {
     val phaseName = "acyclic"
     override val runsBefore = Set("patternMatcher")
 
-    override def run(using Context): Unit = new acyclic.plugin.PluginPhase(cycleReporter, force, fatal).run()
+    override def run(using Context): Unit = {
+      if (!alreadyRun) {
+        alreadyRun = true
+        new acyclic.plugin.PluginPhase(cycleReporter, force, fatal).run()
+      }
+    }
   }
 
   override def init(options: List[String]): List[PluginPhase] = {
