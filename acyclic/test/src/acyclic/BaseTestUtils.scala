2645ab354df58e9176a07bfa91acb8305ddc574e
Tobias Roeser <le.petit.fou@web.de>
2025-03-08 09:47:23 +0100

    comments

--- acyclic/test/src/acyclic/BaseTestUtils.scala
+++ acyclic/test/src/acyclic/BaseTestUtils.scala
@@ -35,19 +35,22 @@ abstract class BaseTestUtils {
       .split(java.io.File.pathSeparator)
       .toIndexedSeq
       .flatMap { f =>
-        val extra = for {
-          manifest <- Option.when(f.endsWith(".jar"))(new JarFile(f).getManifest()).toSeq
-          mainAttr <- Option(manifest.getMainAttributes()).toSeq
-          cp <-Option(mainAttr.getValue("Class-Path")).toSeq
-          entry <- cp.split(" ")
-          if entry.nonEmpty
-        } yield entry match {
-          case url if url.startsWith("file:///") =>
-            url.substring("file://".length)
-          case url if url.startsWith("file:/") =>
-            url.substring("file:".length)
-          case s => s
-        }
+        // If an (empty) classpath pathing jar is used, we extract the `Class-Path` manifest entry
+        // and those entries to the classpath
+        val extra =
+          for {
+            manifest <- Option.when(f.endsWith(".jar"))(new JarFile(f).getManifest()).toSeq
+            mainAttr <- Option(manifest.getMainAttributes()).toSeq
+            cp <- Option(mainAttr.getValue("Class-Path")).toSeq
+            entry <- cp.split(" ")
+            if entry.nonEmpty
+          } yield entry match {
+            case url if url.startsWith("file:///") =>
+              url.substring("file://".length)
+            case url if url.startsWith("file:/") =>
+              url.substring("file:".length)
+            case s => s
+          }
         Seq(f) ++ extra
       }
   }
